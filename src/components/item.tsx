3c8797a1a83a229edce99c08a5e272ea42fbc29a
Irina Sorokina <irasoro402@yandex.ru>
2024-03-03 22:37:47 +0400

    Added prettier check

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -4,8 +4,8 @@ import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 import DeleteIcon from "@mui/icons-material/Delete";
 import EditIcon from "@mui/icons-material/Edit";
@@ -36,7 +36,11 @@ const Item = () => {
 export default function ItemsGrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
         {Array.from(Array(4)).map((_, index) => (
           <Grid item xs={12} sm={4} md={4} key={index}>
             <Item />
