945c9649b144a6cd21163383dea78f30d4dec51e
Irina Sorokina <irasoro402@yandex.ru>
2024-03-22 00:03:59 +0400

    `fmt` image api

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -5,25 +5,35 @@ export async function POST(req: NextRequest) {
   const form = await req.formData();
 
   if (!form.has("image")) {
-    return Response.json({ status: "'image' field is null"}, {
-      status: 500,
-    });
+    return Response.json(
+      { status: "'image' field is null" },
+      {
+        status: 500,
+      },
+    );
   }
 
   const filename = form.get("filename") as string;
   const file = form.get("image") as File;
 
-  console.log(filename);
-
   if (!file) {
-    return Response.json({ status: "image data is null"}, {
-      status: 500
-    });
+    return Response.json(
+      { status: "image data is null" },
+      {
+        status: 500,
+      },
+    );
   }
 
-  await fs.writeFile("some_file.jpg", new Uint8Array(await file.arrayBuffer()));
+  await fs.writeFile(
+    `data/${filename}`,
+    new Uint8Array(await file.arrayBuffer()),
+  );
 
-  return Response.json({ status: "Success" }, {
-    status: 200,
-  });
+  return Response.json(
+    { status: "Success" },
+    {
+      status: 200,
+    },
+  );
 }
