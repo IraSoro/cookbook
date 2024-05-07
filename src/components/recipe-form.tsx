1290dbf4ec06e37bb829b3a322490b292a01c8bd
Irina Sorokina <irasoro402@yandex.ru>
2024-05-08 01:21:19 +0400

    Added some details about recipe to the right of the image

--- src/components/recipe-form.tsx
+++ src/components/recipe-form.tsx
@@ -1,13 +1,31 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { useState } from "react";
+import {
+  Box,
+  Chip,
+  Container,
+  Grid,
+  Stack,
+  Typography,
+  Avatar,
+  IconButton,
+} from "@mui/material";
 import Image from "next/image";
 
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
 import { Item } from "./item";
+import { TempItem } from "../../resources/tempItem";
 
 interface ItemProps {
   item: Item;
 }
 
 const Recipe = ({ item }: ItemProps) => {
+  const [liked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(TempItem.likes);
+
   return (
     <Container>
       <Stack spacing={2} alignItems="center">
@@ -27,29 +45,93 @@ const Recipe = ({ item }: ItemProps) => {
         >
           {item.name}
         </Typography>
-        {item.image && (
-          <Box
-            sx={{
-              height: 450,
-              maxWidth: 350,
-              "@media (max-width: 600px)": {
-                maxWidth: 290,
-              },
-            }}
-          >
-            <Image
-              style={{
-                width: "100%",
-                height: "100%",
-              }}
-              height={0}
-              width={0}
-              src={`/data/${item.image}`}
-              alt={item.name}
-              loading="lazy"
-            />
-          </Box>
-        )}
+        <Grid container alignItems="center" justifyContent="center">
+          <Grid item>
+            <Avatar sx={{ width: 32, height: 32, marginRight: "10px" }} />
+          </Grid>
+          <Grid item>
+            <Typography variant="body1" color="textSecondary">
+              {TempItem.username}
+            </Typography>
+          </Grid>
+        </Grid>
+
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6}>
+            {item.image && (
+              <Box
+                sx={{
+                  height: 450,
+                  maxWidth: 350,
+                  "@media (max-width: 600px)": {
+                    maxWidth: 290,
+                  },
+                }}
+              >
+                <Image
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                  }}
+                  height={0}
+                  width={0}
+                  src={`/data/${item.image}`}
+                  alt={item.name}
+                  loading="lazy"
+                />
+              </Box>
+            )}
+          </Grid>
+
+          <Grid item xs={12} sm={6}>
+            <Grid
+              container
+              alignItems="center"
+              justifyContent="flex-end"
+              sx={{ marginBottom: "20px" }}
+            >
+              <Grid item>
+                <IconButton
+                  onClick={() => {
+                    if (liked) {
+                      setLikesCount((prevCount) => prevCount - 1);
+                    } else {
+                      setLikesCount((prevCount) => prevCount + 1);
+                    }
+                    setLiked(!liked);
+                  }}
+                  color="default"
+                >
+                  {liked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+                </IconButton>
+              </Grid>
+              <Grid item>
+                <Typography variant="body1">{likesCount}</Typography>
+              </Grid>
+            </Grid>
+
+            <Grid item>
+              <Chip
+                icon={<AccessTimeIcon />}
+                label={TempItem.cookingTime}
+                variant="outlined"
+                sx={{
+                  border: 2,
+                  borderColor: "#808080",
+                  fontWeight: "600",
+                  color: "#808080",
+                  marginBottom: "20px",
+                }}
+              />
+            </Grid>
+
+            <Grid container>
+              {TempItem.tags.map((tag, index) => (
+                <Chip key={index} label={tag} style={{ margin: "5px" }} />
+              ))}
+            </Grid>
+          </Grid>
+        </Grid>
         <Typography variant="body1" textAlign="center" color="text.secondary">
           {item.description}
         </Typography>
