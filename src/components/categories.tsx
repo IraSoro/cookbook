78c356eaaaba509d852ebb4054758c1179537472
Irina Sorokina <irasoro402@yandex.ru>
2024-05-14 23:17:34 +0400

    The `Dialog` is separated into a separate component

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -11,6 +11,77 @@ import {
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
+interface CategoriesDialogProps {
+  categories: string[];
+
+  open: boolean;
+  setOpen: (_open: boolean) => void;
+}
+
+const CategoriesDialog = (props: CategoriesDialogProps) => {
+  return (
+    <Dialog
+      fullWidth
+      open={props.open}
+      onClose={() => {
+        props.setOpen(false);
+      }}
+    >
+      <DialogTitle>All Categories</DialogTitle>
+      <DialogContent>
+        <Button
+          variant="outlined"
+          fullWidth
+          startIcon={<AddIcon />}
+          onClick={() => {}}
+          style={{
+            height: "50px",
+            marginBottom: "16px",
+            borderRadius: "8px",
+          }}
+        >
+          Add new category
+        </Button>
+        <Grid container spacing={1}>
+          {props.categories.map((category, index) => (
+            <Grid
+              item
+              xs={6}
+              sm={6}
+              key={index}
+              onClick={() => {
+                console.log("category");
+              }}
+            >
+              <div
+                style={{
+                  height: "50px",
+                  backgroundColor: "#e0e0e0",
+                  borderRadius: "8px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Typography>{category}</Typography>
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+      </DialogContent>
+      <DialogActions>
+        <Button
+          onClick={() => {
+            props.setOpen(false);
+          }}
+        >
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
 interface CategoriesProps {
   categories: string[];
 }
@@ -58,66 +129,12 @@ const Categories = (props: CategoriesProps) => {
             </Button>
           </div>
         ))}
+        <CategoriesDialog
+          categories={props.categories}
+          open={dialogOpen}
+          setOpen={setDialogOpen}
+        />
       </Grid>
-      <Dialog
-        fullWidth
-        open={dialogOpen}
-        onClose={() => {
-          setDialogOpen(false);
-        }}
-      >
-        <DialogTitle>All Categories</DialogTitle>
-        <DialogContent>
-          <Button
-            variant="outlined"
-            fullWidth
-            startIcon={<AddIcon />}
-            onClick={() => {}}
-            style={{
-              height: "50px",
-              marginBottom: "16px",
-              borderRadius: "8px",
-            }}
-          >
-            Add new category
-          </Button>
-          <Grid container spacing={1}>
-            {props.categories.map((category, index) => (
-              <Grid
-                item
-                xs={6}
-                sm={6}
-                key={index}
-                onClick={() => {
-                  console.log("category");
-                }}
-              >
-                <div
-                  style={{
-                    height: "50px",
-                    backgroundColor: "#e0e0e0",
-                    borderRadius: "8px",
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                  }}
-                >
-                  <Typography>{category}</Typography>
-                </div>
-              </Grid>
-            ))}
-          </Grid>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={() => {
-              setDialogOpen(false);
-            }}
-          >
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
     </>
   );
 };
