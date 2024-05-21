b32228589b8b606ae51a9c9f1ba69db677a9a16a
Irina Sorokina <irasoro402@yandex.ru>
2024-05-21 14:11:09 +0300

    Added `add categories button` and `see all button`

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -1,4 +1,6 @@
-import { Typography, Grid, Link, Stack } from "@mui/material";
+import { useState } from "react";
+
+import { Typography, Grid, Link, Stack, Button } from "@mui/material";
 
 import EggAltIcon from "@mui/icons-material/EggAlt";
 import SoupKitchenIcon from "@mui/icons-material/SoupKitchen";
@@ -7,6 +9,10 @@ import CookieIcon from "@mui/icons-material/Cookie";
 import FreeBreakfastIcon from "@mui/icons-material/FreeBreakfast";
 import LocalPizzaIcon from "@mui/icons-material/LocalPizza";
 import RestaurantMenuIcon from "@mui/icons-material/RestaurantMenu";
+
+import AddIcon from "@mui/icons-material/Add";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 
 interface IconCategoryProps {
   category: string;
@@ -36,12 +42,64 @@ interface CategoriesProps {
 }
 
 const Categories = (props: CategoriesProps) => {
+  const defaultLimitCategories = 3;
+  const [limitCategories, setLimitCategories] = useState(
+    defaultLimitCategories
+  );
+  const [isSeeAll, setIsSeeAll] = useState(false);
+
   return (
     <>
-      <Typography variant="h6">Categories</Typography>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          marginBottom: "20px",
+        }}
+      >
+        <Typography variant="h6">Categories</Typography>
+        <Button
+          variant="text"
+          style={{ backgroundColor: "transparent" }}
+          endIcon={isSeeAll ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          onClick={() => {
+            if (isSeeAll) {
+              setIsSeeAll(false);
+              setLimitCategories(defaultLimitCategories);
+              return;
+            }
+            setIsSeeAll(true);
+            setLimitCategories(props.categories.length);
+          }}
+        >
+          See All
+        </Button>
+      </div>
       <Grid container spacing={1}>
-        {props.categories.map((category, index) => (
-          <Grid item xs={6} sm={4} key={index}>
+        <Grid item xs={6} sm={3} key="button">
+          <div
+            style={{
+              height: "50px",
+              backgroundColor: "transparent",
+              borderRadius: "8px",
+              border: "2px solid",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              startIcon={<AddIcon />}
+              variant="text"
+              style={{ color: "#000" }}
+              onClick={() => {}}
+            >
+              Add new
+            </Button>
+          </div>
+        </Grid>
+        {props.categories.slice(0, limitCategories).map((category, index) => (
+          <Grid item xs={6} sm={3} key={index}>
             <div
               style={{
                 height: "50px",
