4de30641b408f51aa36b724bad70a45ca1bc01f8
Irina Sorokina <irasoro402@yandex.ru>
2024-06-21 23:57:13 +0400

    The image has been uploaded to the storage (post)

--- src/pages/api/routes/images.js
+++ src/pages/api/routes/images.js
@@ -2,47 +2,63 @@ import { NextResponse, NextRequest } from "next/server";
 
 import { createClient } from "@supabase/supabase-js";
 
+import formidable from "formidable";
+import { promises as fs } from "fs";
+
+// TODO: replace this to separate file (for example lib/supabaseClient.js)
 const supabaseUrl = process.env.PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.PUBLIC_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default async function handler(req, res) {
   try {
     switch (req.method) {
       case "POST": {
-        const form = await req.formData();
+        const form = formidable({ multiples: true });
 
+        form.parse(req, async (err, fields, files) => {
+          if (err) {
+            console.log("Error parsing the files:", err);
+            return res.status(500).json({ error: "Error parsing the files" });
+          }
 
-        if (!form.has("image")) {
-          return NextResponse.json(
-            { status: "'image' field is null" },
-            { status: 501 }
-          );
-        }
+          const filename = fields.filename;
+          const file = files.image[0];
 
-        const filename = form.get("filename");
-        const file = form.get("image");
+          if (!file) {
+            return res.status(400).json({ error: "No file uploaded" });
+          }
+          const filePath = file.filepath;
+          try {
+            const fileData = await fs.readFile(filePath);
+            const uploadPath = `public/${filename}`;
 
-        if (!file) {
-          return NextRequest.json(
-            { status: "image data is null" },
-            { status: 502 }
-          );
-        }
+            const { error } = await supabase.storage
+              .from("cookbook-image")
+              .upload(uploadPath, fileData, {
+                contentType: file.mimetype,
+              });
 
-        const { error } = await supabase.storage
-          .from("cookbook-image")
-          .upload(`public/${filename}`, file);
+            if (error) {
+              console.log("Error uploading to Supabase:", error.message);
+              return res.status(500).json({ error: `Error: ${error.message}` });
+            }
 
-        if (error) {
-          return NextRequest.json(
-            { status: `Error ${error.message}` },
-            { status: 503 }
-          );
-        }
-
-        return NextResponse.json({ status: "Success" }, { status: 200 });
+            return res
+              .status(200)
+              .json({ message: "Image uploaded successfully" });
+          } catch (error) {
+            console.log("Error reading file:", error.message);
+            return res.status(500).json({ error: "Internal server error" });
+          }
+        });
       }
       case "DELETE": {
       }
@@ -53,7 +69,8 @@ export default async function handler(req, res) {
       }
     }
   } catch (err) {
-    res.status(500).json({ error: err });
+    console.log("err = ", err);
+    res.status(400).json({ error: err });
   }
 }
 
